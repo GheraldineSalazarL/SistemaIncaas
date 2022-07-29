a74108b87a78312c262c49656634ee7f89656a78
gherals <gh.eeraldin@gmail.com>
2022-07-29 16:21:03 -0300

    1.2 Admin Usuario

--- js/adminUsuario.js
+++ js/adminUsuario.js
@@ -20,6 +20,11 @@ let inputCelUs=document.getElementById('inputCelUs');
 let btnCrearUser = document.getElementById("btnCrearUser");
 let mensjFormUs = document.getElementById("mensjFormUs");
 
+let inputRolUsM=document.getElementById('inputRolUsM');
+let inputNombreUsM=document.getElementById('inputNombreUsM');
+let inputEmailUsM=document.getElementById('inputEmailUsM');
+let inputCelUsM=document.getElementById('inputCelUsM');
+
 
 //  Boton Desplegar formulario de nuevo usuario
 let section = true;
@@ -45,7 +50,7 @@ btnCrearUser.onclick = (e) =>{
     let usuario=inputUs.value;
     const buscado = usuarios.find(usuarioBus=>usuarioBus.usuario===usuario);
     if (buscado){
-        // resetInputs();
+        resetInputs();
         Swal.fire({
             title: 'Alerta',
             text: 'El usuario ya existe',
@@ -83,11 +88,11 @@ const addUs = () =>{
         mensjFormUs.innerHTML = "¡Ingrese el nombre del usuario!";
         setTimeout(() => {mensjFormUs.innerHTML = "";}, 3000);
     }else{
-        let nuevoUsuario = new listaUsuarios(usuario,rol,nombre,email);
+        let nuevoUsuario = new listaUsuarios(usuario,rol,nombre,email,cel);
         usuarios.push(nuevoUsuario);
         localStorage.setItem("usuarios", JSON.stringify(usuarios));
         
-        // resetInputs();
+        resetInputs();
         Swal.fire({
             title: 'Bien hecho!',
             text: 'Nuevo usuario registrado con éxito',
@@ -95,9 +100,127 @@ const addUs = () =>{
             showConfirmButton: false,
             timer: 3000,
         });
-        // addNewListaUsuarios();
+        addNewListaUsuarios();
         // addDoc();
         // inicialize();
     }
 }
+
+// Limpiar formulario
+const resetInputs = () =>{
+    inputUs.value ="";
+    inputRolUs.value ="";
+    inputNombreUs.value ="";
+    inputEmailUs.value ="";
+    inputCelUs.value ="";
+}
+
+// Agregar lista en DOM
+const addNewListaUsuarios = () => {
+    tablaUs.innerHTML = "";
+    let tr;
+
+    usuarios.forEach((element,i) => {
+        tr = document.createElement('tr');
+        tr.setAttribute("id",i);
+
+        tr.innerHTML = `<td>${i+1}</td>
+                        <td>${element.usuario}</td>
+                        <td>${element.nombre}</td>
+                        <td>${element.cel}</td>
+                        <td>${element.email}</td>
+                        <td>${element.rol}</td>
+                        <td> </td>
+                        <td>
+                            <a class="btn btn-danger btn-circle btn-sm" onClick="deleteClick(${i})">
+                                 <i class="fas fa-trash"></i>
+                            </a>
+                            <a class="btn btn-warning btn-circle btn-sm" onClick="modificarClick(${i})">
+                                 <i class="fas fa-pen"></i>
+                            </a>
+                        </td>`;     
+        tablaUs.appendChild(tr);
+    });
+}
+usuarios.length > 0 && addNewListaUsuarios(); 
+
+// Eliminar usuario
+deleteClick = (index) =>{
+    let deletedUs = usuarios.splice(index, 1); 
+    localStorage.setItem("usuarios",JSON.stringify(usuarios));
+    addNewListaUsuarios(); 
+    // inicialize(); // actualiza los elementos del Dashboard
+}
+
+// Modificar usuario
+modificarClick = (i) =>{
+    userModif = usuarios.slice(i,i+1);
+    userModifUsuario=userModif[0].usuario;
+ 
+    Swal.fire({
+        title: 'Modificar Usuario',
+        html: `<div class="card-body col-md-12 m-auto card shadow">
+            <form class="col-md-12 text-left row g-3">
+                    <div class="col-md-6">
+                        <label class="col-md-12 pl-0 ">Usuario</label>
+                        <input type="text" class="form-control" value="${userModifUsuario}" disabled>
+                        </div>
+                    <div class="col-md-6">
+                        <label for="inputRolUsM" class="form-label">Rol</label>
+                        <select class="form-control" id="inputRolUsM" required>
+                            <option selected disabled value="">Seleccione</option>
+                            <option value="Administrador">Administrador</option>
+                            <option value="Docente">Docente</option>
+                            <option value="Asesor">Asesor</option>
+                        </select>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="inputNombreUsM" class="form-label pt-2">Nombre Completo</label>
+                        <input id="inputNombreUsM" type="text" class="form-control" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputEmailUsM" class="form-label pt-2">Email</label>
+                        <input id="inputEmailUsM" type="email" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputCelUsM" class="form-label pt-2">Celular</label>
+                        <input id="inputCelUsM" type="cel" class="form-control">
+                    </div>
+                </form>
+            </div>`,
+            confirmButtonText: 'Modificar Usuario',
+            focusConfirm: false,
+            preConfirm:() => {
+                const rolM = Swal.getPopup().querySelector('#inputRolUsM').value
+                const nombreM = Swal.getPopup().querySelector('#inputNombreUsM').value
+                const emailM = Swal.getPopup().querySelector('#inputEmailUsM').value
+                const celM = Swal.getPopup().querySelector('#inputCelUsM').value
+                return{rolM:rolM, nombreM:nombreM, emailM:emailM, celM:celM}
+            }
+        }).then((result) => {
+            if(result.isConfirmed){
+                const buscado = usuarios.find(usuarioBus=>usuarioBus.usuario===userModifUsuario);
+                if (buscado){
+                    if(result.value.rolM !== "Seleccione" && result.value.rolM !== ''){buscado.rol = result.value.rolM;}
+                    if(result.value.nombreM !== ''){buscado.nombre = result.value.nombreM;}
+                    if(result.value.emailM !== ''){buscado.email = result.value.emailM;}
+                    if(result.value.celM !== ''){buscado.cel = result.value.celM;}
+        
+                    localStorage.setItem("usuarios",JSON.stringify(usuarios));
+                    addNewListaUsuarios();
+                    resetInputs();
+                    Swal.fire({
+                        title: 'Bien hecho!',
+                        text: 'Usuario modificado con éxito',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2500
+                    });
+                }
+            }
+            
+        })
+}
+
+
 
